5cec67ed95c349a00b65d795edeae4f6953affe3
Hung Pham <hungpham2511@gmail.com>
2018-09-15 12:06:52 +0800

    various bug fixed. first ran on robot. position only.

--- include/infinite_interaction_lib.h
+++ include/infinite_interaction_lib.h
@@ -11,30 +11,164 @@
 #include <sensor_msgs/JointState.h>
 #include <vector>
 
-class FTSensorHandler {
-public:
-    double fx, fy, fz;
-    FTSensorHandler(): fx(0), fy(0), fz(0) {};
-    void signal_callback(const geometry_msgs::WrenchStampedConstPtr &msg);
+typedef std::vector<double> dVector;
 
+class DiscreteTimeFilter {
+
+    /*! Order of the filter. Is basically the order of the denominator. */
+    int order;
+    /*! Current index. */
+    int n;
+    /*! The last (order)-th input and output is stored. All are default to some initial condition. */
+    dVector x_mem, y_mem;
+    dVector cof_a, cof_b;
+public:
+    /*! Initialize the filter.
+        *
+        * \param cof_a_in numerator
+        * \param cof_b_in denominator
+        * \param y_initial_val initial condition of the output. initial condition of input is default to zero.
+        */
+    DiscreteTimeFilter(dVector &cof_a_in, dVector &cof_b_in, double y_initial_val=0);
+    /*! Compute the output in the next time step.
+     *
+     * ccde: a0 x[n] + a1 x[n-1] + ... a_o x[n-o]  = b0 y[n] + b1 y[n-1] + ... + b_o y[n - o]
+     *
+     * @param x_n
+     * @return y_n
+     */
+    double compute(double x_n);
 };
 
-class JointPositionHandler{
+DiscreteTimeFilter::DiscreteTimeFilter(dVector &cof_a_in, dVector &cof_b_in, double y_initial_val) {
+    n = 0;
+    order = cof_b_in.size() - 1;
+    assert(order >= 0 and cof_b_in[0] > 0);
+
+    x_mem.resize(order + 1);
+    y_mem.resize(order + 1);
+    for (int i = 0; i < order + 1; ++i) {
+        x_mem[i] = 0;
+        y_mem[i] = y_initial_val;
+    }
+
+    // resize cof_a_in to a vector of size (order + 1) and shift it rightward
+    int order_a = cof_a_in.size() - 1;
+    cof_a_in.resize(order + 1);
+    for (int i=order; i >= 0; --i){
+        if (i - (order - order_a) >= 0){
+            cof_a_in[i] = cof_a_in[i - (order - order_a)];
+        }
+        else {
+            cof_a_in[i] = 0;
+        }
+    }
+    cof_a = cof_a_in;
+    cof_b = cof_b_in;
+}
+
+double DiscreteTimeFilter::compute(double x_n) {
+    n++;
+    int deg = order + 1;
+    x_mem[n % deg] = x_n;
+    double sum = 0;
+    for (int i = 0; i < deg; ++i) {
+        sum += cof_a[i] * x_mem[(n - i + deg) % deg];
+    }
+    for (int i = 1; i < deg; ++i){
+        sum -= cof_b[i] * y_mem[(n - i + deg) % deg];
+    }
+    y_mem[n % deg] = sum / cof_b[0];
+    return y_mem[n % deg];
+}
+
+
+/*! A class for handling FT signal stream.
+ *
+ */
+class FTSensorHandler {
+    double fx, fy, fz, tx, ty, tz;
+    std::vector<double> wrench_offset;
 public:
+    FTSensorHandler();;
+    /*! Construct a FT Sensor Handler object.
+     *
+     * abc
+     */
+    explicit FTSensorHandler(const std::vector<double> &wrench_offset_input);;
+    void signal_callback(const geometry_msgs::WrenchStampedConstPtr &msg);
+    void get_latest_wrench(std::vector<double> &force, std::vector<double> &torque);
+//    void set_filter(); not implemented
+};
+
+
+void FTSensorHandler::signal_callback(const geometry_msgs::WrenchStampedConstPtr &msg) {
+    fx = msg->wrench.force.x - wrench_offset[0];
+    fy = msg->wrench.force.y - wrench_offset[1];
+    fz = msg->wrench.force.z - wrench_offset[2];
+    tx = msg->wrench.torque.x - wrench_offset[3];
+    ty = msg->wrench.torque.y - wrench_offset[4];
+    tz = msg->wrench.torque.z - wrench_offset[5];
+}
+
+void FTSensorHandler::get_latest_wrench(std::vector<double> &force, std::vector<double> &torque) {
+    force.resize(3);
+    torque.resize(3);
+    force[0] = fx;
+    force[1] = fy;
+    force[2] = fz;
+    torque[0] = tx;
+    torque[1] = ty;
+    torque[2] = tz;
+}
+
+FTSensorHandler::FTSensorHandler(const std::vector<double> &wrench_offset_input) :  fx(0), fy(0), fz(0), tx(0), ty(0), tz(0) {
+    wrench_offset.resize(6);
+    for (int i = 0; i < 6; ++i) {
+        wrench_offset[i] = wrench_offset_input[i];
+    }
+}
+
+FTSensorHandler::FTSensorHandler() : fx(0), fy(0), fz(0), tx(0), ty(0), tz(0) {
+    wrench_offset.resize(6);
+    for (int i = 0; i < 6; ++i) {
+        wrench_offset[i] = 0;
+    }
+}
+
+class JointPositionHandler{
     std::vector<double> joint_position;
+public:
     JointPositionHandler();
     void signal_callback(const sensor_msgs::JointStateConstPtr &msg);
-
+    dVector& get_latest_jnt_position();
+    bool received_msg();
 };
 
 JointPositionHandler::JointPositionHandler() {
     joint_position.resize(6);
+    for (int i = 0; i < 6; ++i) {
+        joint_position[i] = -999; // coded, should changed if have received message
+    }
 }
 
 void JointPositionHandler::signal_callback(const sensor_msgs::JointStateConstPtr &msg) {
     for (int i = 0; i < 6; ++i) {
         joint_position[i] = msg->position[i];
     }
+}
+
+dVector &JointPositionHandler::get_latest_jnt_position() {
+    return joint_position;
+}
+
+bool JointPositionHandler::received_msg() {
+    for (int i=0; i < 6; ++i){
+        if (joint_position[i] == -999){
+            return false;
+        }
+    }
+    return true;
 }
 
 class JointPositionController {
@@ -67,10 +201,26 @@ void JointPositionController::set_joint_positions(std::vector<double>& jnt_posit
     }
 }
 
-void FTSensorHandler::signal_callback(const geometry_msgs::WrenchStampedConstPtr &msg) {
-    fx = msg->wrench.force.x;
-    fy = msg->wrench.force.y;
-    fz = msg->wrench.force.z;
-
+void matrix_mult(std::vector<double> &A, std::vector<double> &x, std::vector<double> &y){
+    unsigned int size_y = A.size() / x.size(), size_x = x.size();
+    y.resize(size_y);
+    assert(A.size() % x.size() == 0);
+    for (int i = 0; i < size_y; ++i) {
+        y[i] = 0;
+        for (int j = 0; j < size_x; ++j) {
+            y[i] += A[i * size_x + j] * x[j];
+        }
+    }
 }
+
+void matrix_add(std::vector<double> &x, std::vector<double> &y, std::vector<double> &z){
+    assert(x.size() == y.size());
+    z.resize(x.size());
+    for (int i = 0; i < x.size(); ++i) {
+        z[i] = x[i] + y[i];
+    }
+}
+
+
+
 #endif //PROJECT_INFINITE_INTERACTION_LIB_H
--- src/admittance_controller.cpp
+++ src/admittance_controller.cpp
@@ -29,7 +29,7 @@ void SetViewer(OpenRAVE::EnvironmentBasePtr penv, const std::string& viewername)
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "admittance_node");
+    ros::init(argc, argv, "admittance_control");
     ros::NodeHandle node_handle("~");
     // parameter
     std::string name_space = "denso";
@@ -38,34 +38,101 @@ int main(int argc, char **argv)
     std::string scenefilename = "robots/denso_handle.robot.xml";
     std::string viewername = "qtosg";
     std::string robotname = "denso_handle";
-    // FTSensor
-    FTSensorHandler ft_handler;
+    std::vector<double> wrench_ref;
+    node_handle.getParam("ft_sensor_ref", wrench_ref);
+    std::vector<double> position_ref;
+    node_handle.getParam("joint_pos_ref", position_ref);
+    if (wrench_ref.size() != 6){
+        ROS_ERROR_STREAM("Reference wrench from param sever is invalid! Have you loaded the parameters? \n -- Exitting!");
+        exit(0);
+    }
+    if (position_ref.size() != 6){
+        ROS_ERROR_STREAM("Reference position from param sever is invalid! Have you loaded the parameters? \n -- Exitting!");
+        exit(0);
+    }
+    // FTsensor handler
+    FTSensorHandler ft_handler(wrench_ref);
     ros::Subscriber ft_sub = node_handle.subscribe(ft_sensor_topic, 3, &FTSensorHandler::signal_callback, &ft_handler);
-    // position handler and command
+    // position handler
     JointPositionHandler position_handler;
     ros::Subscriber position_handler_sub = node_handle.subscribe(joint_state_topic, 3, &JointPositionHandler::signal_callback, &position_handler);
-    // command
-    JointPositionController position_ctrl(name_space, node_handle);
-    std::vector<double> position_cmd = {0, 0, 0, 0, 0, 0};
+    ros::Duration(0.5).sleep(); ros::spinOnce(); // update current joint position
+    if (!position_handler.received_msg()){
+        ROS_FATAL("Have not received messages to update initial joint position! \n-- Terminating!");
+        exit(0);
+    }
+    // position controller and command
+    JointPositionController position_act(name_space, node_handle);
+    dVector initial_jnt_position = position_handler.get_latest_jnt_position();
+    ROS_INFO_STREAM("Initial position: " << initial_jnt_position[0] << ", " << initial_jnt_position[1] << ", "
+                            << initial_jnt_position[2] << ", " << initial_jnt_position[3] << ", "
+                            << initial_jnt_position[4] << ", " << initial_jnt_position[5]);
+    // position command
+    std::vector<DiscreteTimeFilter> admittance_filters;
+    for (int i = 0; i < 6; ++i) {
+        dVector cof_a, cof_b;
+        node_handle.getParam("j" + std::to_string(i + 1) + "/tf_f2q_a", cof_a);
+        node_handle.getParam("j" + std::to_string(i + 1) + "/tf_f2q_b", cof_b);
+        double jnt_initial_pos = position_handler.get_latest_jnt_position()[i] - position_ref[i];
+        DiscreteTimeFilter filter_(cof_a, cof_b, jnt_initial_pos);
+        admittance_filters.push_back(filter_);
+    }
     // openrave
     OpenRAVE::RaveInitialize(true); // start openrave core
     OpenRAVE::EnvironmentBasePtr penv = OpenRAVE::RaveCreateEnvironment(); // create the main environment
-    OpenRAVE::RaveSetDebugLevel(OpenRAVE::Level_Debug);
+    OpenRAVE::RaveSetDebugLevel(OpenRAVE::Level_Info);
     boost::thread thviewer(boost::bind(SetViewer,penv,viewername));
     penv->Load(scenefilename); // load the scene
     OpenRAVE::RobotBasePtr robot;
     robot = penv->GetRobot(robotname);
+    auto manip = robot->GetActiveManipulator();
     ros::Rate rate(125);
+    // temp variable
+    std::vector<double> position_cmd = {0, 0, 0, 0, 0, 0};
+    std::vector<double> wrench = {0, 0, 0, 0, 0, 0};
+    std::vector<double> tau_prj(6), tau_prj1(6), tau_prj2(6), force(3), torque(3);
+    std::vector<double> jacobian, jacobian_rot;
+    // controller
     while(!ros::isShuttingDown()){
+        auto tstart = ros::Time::now();
+        // call all callbacks
+        ros::spinOnce();
         // retrieve the latest wrench measurement, offseted and filtered
+        ft_handler.get_latest_wrench(force, torque);
 
-        // project the wrench to the joint space
-        robot->SetActiveDOFValues(position_handler.joint_position);
+        // project to joint torque space
+        robot->SetActiveDOFValues(position_handler.get_latest_jnt_position());
+        manip->CalculateJacobian(jacobian);
+        manip->CalculateAngularVelocityJacobian(jacobian_rot);
 
-        // run the wrench through the joint filter
+        matrix_mult(jacobian, force, tau_prj1);
+        matrix_mult(jacobian_rot, torque, tau_prj2);
+        matrix_add(tau_prj1, tau_prj2, tau_prj);
+
+        // run through joint position filter
+        for (int i = 0; i < 6; ++i) {
+            double y_ = admittance_filters[i].compute(tau_prj[i]);
+            position_cmd[i] = position_ref[i] + y_;
+            if (i==0){
+                ROS_DEBUG_STREAM("j0: y_=" << y_);
+            }
+        }
+        // send command
+        position_act.set_joint_positions(position_cmd);
+        // record time required
+        auto tend = ros::Time::now();
+        ros::Duration tdur = tend - tstart;
+        // report, clean up then sleep
+        ROS_DEBUG_STREAM("force: " << force[0] << ", " << force[1] << ", " << force[2]);
+        ROS_DEBUG_STREAM("torque: " << torque[0] << ", " << torque[1] << ", " << torque[2]);
+        ROS_DEBUG_STREAM("tau: " << tau_prj[0] << ","
+                                 << tau_prj[1] << ","
+                                 << tau_prj[2] << ","
+                                 << tau_prj[3] << ","
+                                 << tau_prj[4] << ","
+                                 << tau_prj[5]);
+        ROS_DEBUG_STREAM("comp time: " << tdur.toSec() * 1000 << "ms");
         rate.sleep();
-        ROS_DEBUG_STREAM("In control loop " << position_handler.joint_position[0]);
-        ROS_DEBUG_STREAM(ft_handler.fx << ft_handler.fy);
     }
 
   return 0;
