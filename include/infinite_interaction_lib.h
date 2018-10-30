477e83dc87ab79508bdc819beef32e88d80d0407
Hung Pham <hungpham2511@gmail.com>
2018-10-30 12:38:45 +0800

    implement FIR series+feedback controller with test

--- include/infinite_interaction_lib.h
+++ include/infinite_interaction_lib.h
@@ -11,8 +11,118 @@
 #include <std_msgs/Header.h>
 #include <sensor_msgs/JointState.h>
 #include <vector>
+#include <algorithm>
 
 typedef std::vector<double> dVector;
+
+
+
+
+/*! Discrete-time MIMO FIR series+feedback (srfb) controller.
+ *
+ * The filter receives a stream of vector-valued input y[0], y[1], ...
+ * and produces a stream of vector-valued output u[0], u[1], ...
+ *
+ * The input and output streams are stored in respectively in two
+ * circular input and output banks ybank and ubank. New data is
+ * added to the bank reversely. See below for an example
+ *
+ * y[n][0], y[n][1], y[n][2], ..., y[1][0], y[1][1], y[1][2], y[0][0], y[0][1], y[0][2]
+ *
+ * The input bank ybank has length ny * T; the output bank has length nu * T.
+ * This is in order for the banks to contain T periods of input output.
+ *
+ * Filter coefficient vector L has shape (L, ny, nu). Filter coefficient vector MB2 has shape (L, nu, nu).
+ *
+ * At each call to `compute`:
+ *   1. an input y[n] is fed to the controller,
+ *   2. yidx point to y[n-1][0],
+ *   3. uidx point to u[n-1][0].
+ * Before the call terminates:
+ *   1. an output u[n] is readied to be returned;
+ *   2. yidx point to y[n][0],
+ *   3. uidx point to u[n][0].
+ *
+ * u[n] is computed as follows:
+ *   alpha = L[0] * y[n] + ... + L[T - 1] * y[n - T + 1]
+ *   beta = M[1] * u[n - 1] + ... + M[T - 1] * u[n - T + 1]
+ *   u[n] = alpha - beta
+ */
+class FIRsrfb {
+    unsigned int T, ny, nu, /*FIR banks shape*/
+            yidx = 0, uidx = 0, ybank_sz, ubank_sz;  /*Current index of y[n][0] and u[n][0] respectively.*/
+    dVector L, MB2,   /*FIR coefficients in row-order*/
+            ybank, ubank,  /*Storage banks*/
+            alpha, beta, un;  /*temp*/
+
+public:
+    FIRsrfb(unsigned int T_, unsigned int ny_, unsigned int nu_, dVector L_, dVector MB2_);
+    dVector compute(const dVector & y_n); /*See class docstring*/
+};
+
+
+FIRsrfb::FIRsrfb(unsigned int T_, unsigned int ny_, unsigned int nu_, dVector L_, dVector MB2_) :  T(T_), ny(ny_), nu(nu_), L(L_), MB2(MB2_) {
+    // check shape consistency
+    if (T * ny * nu != L.size() or T * nu * nu != MB2.size()){
+        throw std::invalid_argument("Wrong input shape");
+    }
+    // the 0-th impulse of MB2 is zero
+    for (int i = 0; i < nu * nu; ++i) {
+        if (MB2[i] != 0){
+            throw std::invalid_argument("MB2[0] needs to be zero");
+        }
+    }
+    // initiate ybank and ubank
+    ybank.resize(T * ny);
+    ubank.resize(T * nu);
+    ybank_sz = ybank.size();
+    ubank_sz = ubank.size();
+    std::fill(ybank.begin(), ybank.end(), 0);
+    std::fill(ubank.begin(), ubank.end(), 0);
+    // initiate temporary variable
+    alpha.resize(nu);
+    beta.resize(nu);
+    un.resize(nu);
+}
+
+dVector FIRsrfb::compute(const dVector & y_n){
+    // store y_n to ybank
+    for (unsigned int i = 0; i < ny; ++i) {
+        yidx = (yidx - 1 + ybank_sz) % ybank_sz;
+        ybank[yidx] = y_n[ny - 1 - i];
+    }
+    // compute alpha,
+    std::fill(alpha.begin(), alpha.end(), 0);
+    // (i,j,k) represent three indices of matrix L
+    // since L, shaped (T, nu, ny) is stored row-order, id = k + j * ny + i * nu * ny
+    for (unsigned int i=0; i < T; ++i){
+        for (unsigned int j=0; j < nu; ++j){
+            for (unsigned int k=0; k < ny; ++k){
+                alpha[j] += L[k + j * ny + i * nu * ny] * ybank[(yidx + k + i * ny) % ybank_sz];
+            }
+        }
+    }
+    // beta
+    std::fill(beta.begin(), beta.end(), 0);
+    uidx = (uidx - nu + ubank_sz) % ubank_sz;  // shift current index on ubank
+    for (unsigned int i=1; i < T; ++i){
+        for (unsigned int j=0; j < nu; ++j){
+            for (unsigned int k=0; k < nu; ++k){
+                beta[j] += MB2[k + j * nu + i * nu * nu] * ubank[(uidx + k + i * nu) % ubank_sz];
+            }
+        }
+    }
+    // and finally u_n,
+    for(unsigned int i=0; i < nu; ++i){
+        un[i] = alpha[i] - beta[i];
+        // store u_n to ubank
+        ubank[(uidx + i) % ubank_sz] = un[i];
+    }
+
+    return un;
+}
+
+
 
 /*! Discrete-time SISO filter.
  *
@@ -88,7 +198,8 @@ double DiscreteTimeFilter::compute(double x_n) {
 /*! FT signal stream handler.
  *
  * A handler applies basic operations including collecting data and
- * filter them using the DiscreteTimeFilter class.
+ * filter them using DiscreteTimeFilter objects. Note that there are 6
+ * individual filters, but all have the same coefficients.
  */
 class FTSensorHandler {
     double fx, fy, fz, tx, ty, tz;
@@ -274,7 +385,7 @@ void ExternalTorquePublisher::publish_joint_torques(std::vector<double> &taus) {
     }
 }
 
-
+// y = Ax
 void matrix_mult(std::vector<double> &A, std::vector<double> &x, std::vector<double> &y){
     unsigned int size_y = A.size() / x.size(), size_x = x.size();
     y.resize(size_y);
@@ -287,6 +398,7 @@ void matrix_mult(std::vector<double> &A, std::vector<double> &x, std::vector<dou
     }
 }
 
+// z = x + y
 void matrix_add(std::vector<double> &x, std::vector<double> &y, std::vector<double> &z){
     assert(x.size() == y.size());
     z.resize(x.size());
--- src/tests/test_fir_controller.cpp
+++ src/tests/test_fir_controller.cpp
@@ -11,11 +11,12 @@ TEST(FIR, zeroL_scalar){
     dVector L {0, 0, 0, 0}, MB2{0, -2, 3, 5};
 
     FIRsrfb controller (4, 1, 1, L, MB2);  // 4-steps horizon
-    dVector u{0, 2, 3, 9, 2, 3};
-    double yi;
-    for (int i = 0; i < u.size(); ++i) {
-        yi = controller.compute(u[i]);
-        EXPECT_EQ(yi, 0);
+    dVector yin{0, 2, 3, 9, 2, 3};
+    dVector u_n;
+    for (int i = 0; i < yin.size(); ++i) {
+        dVector y_n{yin[i]};
+        u_n = controller.compute(y_n);
+        EXPECT_EQ(u_n[0], 0);
     }
 }
 
@@ -24,12 +25,12 @@ TEST(FIR, zeroL_multi){
     dVector L {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, MB2{0, 0, 0, 0, -2, 3, 5, 2, 3, 4, 5, 2};
 
     FIRsrfb controller (3, 2, 2, L, MB2);  // 4-steps horizon
-    dVector u1{0, 2, 3, 9, 2, 3}, u2{2, 3, 9, 0, 2, 3}, yi;
-    for (int i = 0; i < u1.size(); ++i) {
-        dVector u{u1[i], u2[i]};
-        yi = controller.compute(u);
-        EXPECT_EQ(yi[0], 0);
-        EXPECT_EQ(yi[1], 0);
+    dVector yin{0, 2, 3, 9, 2, 3}, yin2{2, 3, 9, 0, 2, 3}, u_n;
+    for (int i = 0; i < yin.size(); ++i) {
+        dVector y_n{yin[i], yin2[i]};
+        u_n = controller.compute(y_n);
+        EXPECT_EQ(u_n[0], 0);
+        EXPECT_EQ(u_n[1], 0);
     }
 }
 
@@ -39,59 +40,124 @@ TEST(FIR, zeroMB2_scalar){
     dVector L {2, 0, 0, 0}, MB2{0, 0, 0, 0};
 
     FIRsrfb controller (4, 1, 1, L, MB2);  // 4-steps horizon
-    dVector u{0, 2, 3, 9, 2, 3};
-    double yi;
-    for (int i = 0; i < u.size(); ++i) {
-        yi = controller.compute(u[i]);
-        EXPECT_EQ(yi, 0);
+    dVector yin{0, 2, 3, 9, 2, 3};
+    dVector u_n;
+    for (int i = 0; i < yin.size(); ++i) {
+        dVector y_n{yin[i]};
+        u_n = controller.compute(y_n);
+        EXPECT_EQ(u_n[0], 2 * yin[i]);
+    }
+}
+
+
+// impulse MB2 is zero, expect feedthrough by L
+TEST(FIR, zeroMB2_scalar_case2){
+    // if the L impulse is zero, expect zero output
+    dVector L {1, 1, 0, 0}, MB2{0, 0, 0, 0};
+
+    FIRsrfb controller (4, 1, 1, L, MB2);  // 4-steps horizon
+    dVector yin{0, 2, 3, 9, 2, 3}, udesired{0, 2, 5, 12, 11, 5};
+    dVector u_n;
+    for (int i = 0; i < yin.size(); ++i) {
+        dVector y_n{yin[i]};
+        u_n = controller.compute(y_n);
+        EXPECT_EQ(udesired[i], u_n[0]);
     }
 }
 
 
 // impulse MB2 is zero, expect
 TEST(FIR, zeroMB2_multi){
+
 }
 
 
 // general case, computed using the below python script
 
-// # python script: generate response
-// import SLSsyn as Ss
-// import numpy as np
-// import control as co
-// L = np.array([0, 1, 2, 3], dtype=float).reshape(-1, 1, 1)
-// MB2 = np.array([0, -2, 3, 5], dtype=float).reshape(-1, 1, 1)
-// Lz = Ss.impulse2tf(L, 1)
-// MB2z = Ss.impulse2tf(MB2, 1)
-// CLz = Lz * co.feedback(1, MB2z, sign=-1)
-// u = [0, 2, 3, 9, 2, 3]
-// t, y, x = co.forced_response(CLz, np.arange(len(u)), u)
-// print(y)  # got this: [[ 0.  0.  2. 11. 37. 60.]]
+//# python script: generate response
+//import SLSsyn as Ss
+//import numpy as np
+//import control as co
+//L = np.array([0, 1, 2, 3], dtype=float).reshape(-1, 1, 1)
+//MB2 = np.array([0, -2, 3, 5], dtype=float).reshape(-1, 1, 1)
+//Lz = Ss.impulse2tf(L, 1)
+//MB2z = Ss.impulse2tf(MB2, 1)
+//CLz = Lz * co.feedback(1, MB2z, sign=-1)
+//y = [0, 2, 3, 9, 2, 3, 10, 20, 100]
+//t, u, x = co.forced_response(CLz, np.arange(len(y)), y)
+//print(u)  # got this:  [[   0.    0.    2.   11.   37.   60.  -12. -367. -949.]]
 TEST(FIR, general_scalar){
     dVector L {0, 1, 2, 3}, MB2{0, -2, 3, 5};
-
     FIRsrfb controller (4, 1, 1, L, MB2);  // 4-steps horizon
-    dVector u{0, 2, 3, 9, 2, 3}, ydesired{0, 0, 2, 11, 37, 60};
-    double yi;
-    for (int i = 0; i < u.size(); ++i) {
-        yi = controller.compute(u[i]);
-        EXPECT_EQ(yi, ydesired[i]);
+    dVector y{0, 2, 3, 9, 2, 3, 10, 20, 100}, udesired{0, 0, 2, 11, 37, 60, -12, -367, -949};
+    dVector ui;
+    for (int i = 0; i < y.size(); ++i) {
+        ui = controller.compute(dVector {y[i]});
+        EXPECT_EQ(udesired[i], ui[0]) << "Error at i=" << i;
     }
 }
 
 
-TEST(FIR, general_multi){}
+// general case with multi-dimensional input and output
+// the desired output (udesired) is generated using the below script
+
+// # python script: generate response
+// import SLSsyn as Ss
+// import numpy as np
+// import control as co
+// np.random.seed(0)
+// # T = 4, nu = 3, ny = 2
+// L = np.random.randint(0, 10, size=(4, 3, 2))
+// MB2 = np.random.randint(0, 10, size=(4, 3, 3))
+// MB2[0, :] = 0
+// Lz = Ss.mtf2ss(Ss.impulse2tf(L, 1), minreal=True)
+// MB2z = Ss.mtf2ss(Ss.impulse2tf(MB2, 1), minreal=True)
+// I3 = co.ss([], [], [], np.eye(3), 1)
+// CLz = co.feedback(I3, MB2z, sign=-1) * Lz
+// yin = np.random.randint(0, 10, size=(5, 2))
+// t, uout, x = co.forced_response(CLz, np.arange(yin.shape[0]), yin, transpose=True)
+//
+// # reproduce results
+// alpha0 = np.dot(L[0], yin[0])
+// alpha1 = np.dot(L[0], yin[1]) + np.dot(L[1], yin[0])
+// alpha2 = np.dot(L[0], yin[2]) + np.dot(L[1], yin[1]) + np.dot(L[0], yin[2])
+// beta0 = np.zeros(3)
+// u0 = alpha0 - beta0
+// beta1 = MB2[1].dot(u0)
+// u1 = alpha1 - beta1
+// beta2 = MB2[1].dot(u1) + MB2[2].dot(u0)
+// u2 = alpha2 - beta2
+
+TEST(FIR, general_multi){
+    dVector L {5, 0, 3, 3, 7, 9, 3, 5, 2, 4, 7, 6, 8, 8, 1, 6, 7, 7, 8, 1, 5, 9, 8, 9},
+            MB2 {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 3, 3, 7, 0, 1, 9, 9, 0, 4, 7, 3,
+                 2, 7, 2, 0, 0, 4, 5, 5, 6, 8, 4, 1, 4, 9};
+    std::vector<std::vector<double > > yin = {{8, 1}, {1, 7}, {9, 9}, {3, 6}, {7, 2}},
+    udesired = {{ 4.000000e+01,  2.700000e+01,  6.500000e+01},
+                {-2.820000e+02, -2.650000e+02, -7.360000e+02},
+                { 2.877000e+03,  2.170000e+03,  9.467000e+03},
+                {-3.198000e+04, -1.783600e+04, -1.074580e+05},
+                { 3.392790e+05,  1.484960e+05,  1.162159e+06}};
+    FIRsrfb controller (4, 2, 3, L, MB2);
+    // (T, nu, ny) = (4, 3, 2)
+    for (int i=0; i < yin.size(); ++i){
+        dVector ui = controller.compute(yin[i]);
+        for(int j=0; j < 3; ++j){
+            EXPECT_DOUBLE_EQ(udesired[i][j], ui[j]) << "Fail at i=" << i << ", j=" << j;
+        }
+    }
+}
 
 
 TEST(FIR, badinput_MB2_0_nonzero){
-    dVector L {0, 1, 2, 3}, MB2{0, -2, 3, 5};
+    dVector L {0, 1, 2, 3}, MB2{1, -2};
      EXPECT_THROW(
             {
                 try {
                     FIRsrfb controller (2, 2, 1, L, MB2);  // 4-steps horizon
                 }
                 catch (const std::invalid_argument & e){
-                    EXPECT_STREQ("MB2[0] needs to be zero.", e.what());
+                    EXPECT_STREQ("MB2[0] needs to be zero", e.what());
                     throw;  // simply rethrow the catched expression
                 }
             }, std::invalid_argument);
@@ -114,6 +180,22 @@ TEST(FIR, badinput_wrongshape){
 }
 
 
+
+// throw exception if the input vectors have wrong shape
+TEST(FIR, badinput_wrongshape_L){
+    dVector L (2 * 3 * 2 - 1), MB2 (2 * 2 * 2);
+    int T = 2, ny = 3, nu = 2;
+    EXPECT_THROW(
+            {
+                try {
+                    FIRsrfb controller (T, ny, nu, L, MB2);  // 4-steps horizon
+                }
+                catch (const std::invalid_argument & e){
+                    EXPECT_STREQ("Wrong input shape", e.what());
+                    throw;  // simply rethrow the catched expression
+                }
+            }, std::invalid_argument);
+}
 
 
 
