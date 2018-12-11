74531c02a63fac4fe4075eaaf7d85554bb5b1ecb
Hung Pham <hungpham2511@gmail.com>
2018-12-11 09:54:17 +0800

    [figure] admitance shaping and noise attenuation

--- SLS-scripts/Dec10_figures_compare.py
+++ SLS-scripts/Dec10_figures_compare.py
@@ -47,7 +47,7 @@ def fig_admittance_shaping():
     H_contracted_ideal = co.c2d(co.tf([100, 0], [m_des, b_des, 0 + 100]), Ts)
 
     # fig init
-    fig = plt.figure(figsize=[8, 4])
+    fig = plt.figure(figsize=[7, 3.5])
     ax0 = fig.add_subplot(2, 3, 1)
     ax1 = fig.add_subplot(2, 3, 2, sharex=ax0)
     ax2 = fig.add_subplot(2, 3, 3, sharex=ax0)
@@ -74,10 +74,10 @@ def fig_admittance_shaping():
     ax1.plot(T_sim, y1, label='Q', linestyle='-.')
     ax2.plot(T_sim, y2, label='Q', linestyle='-.')
 
-    # ax0.legend(ncol=3, loc='best', framealpha=1)
-    for ax in [ax0, ax1, ax2]:
-        ax.set_xlabel('Time (sec)')
-    ax0.set_ylabel('Velocity (m/s)')
+    # # ax0.legend(ncol=3, loc='best', framealpha=1)
+    # for ax in [ax0, ax1, ax2]:
+    #     ax.set_xlabel('Time (sec)')
+    # ax0.set_ylabel('Velocity (m/s)')
 
     # freq-response
     freqs = np.logspace(-2, np.log10(np.pi / Ts), 100)
@@ -87,10 +87,7 @@ def fig_admittance_shaping():
     ax5 = fig.add_subplot(2, 3, 6, sharex=ax3, sharey=ax3)
     for ax in [ax3, ax4, ax5]:
         ax.set_xscale('log')
-        ax.set_yscale('log')
-        ax.set_ylim([1e-3, 30])
-        ax.set_xlabel('Frequency (rad/s)')
-    ax3.set_ylabel('Magnitude')
+        ax.set_xlim([3e-1, 60])
 
     mag0, _, _ = H_relaxed_ideal.freqresp(freqs)
     mag1, _, _ = H_nom_ideal.freqresp(freqs)
@@ -116,8 +113,110 @@ def fig_admittance_shaping():
     # post-process
     for ax in [ax0, ax1, ax2, ax3, ax4, ax5]:
         ax.grid(which='major')
+ 
+    # hide x labels
+    plt.setp(ax1.get_yticklabels(), visible=False)
+    plt.setp(ax2.get_yticklabels(), visible=False)
+    plt.setp(ax4.get_yticklabels(), visible=False)
+    plt.setp(ax5.get_yticklabels(), visible=False)
+   
     plt.tight_layout()
+    plt.savefig("Dec10_admittance_shaping.pdf")
     plt.show()
+
+
+def fig_noise_attenuation():
+    Pz_nom = plantMdelta(
+        E_gain=50, wI=1.0, sys_type='33_mult_unt', m_int=0.1, N_in=1, N_out=1)
+    Pz_contracted = plantMdelta(
+        E_gain=100, wI=1.0, sys_type='33_mult_unt', m_int=0.1, N_in=1, N_out=1)
+    Pz_relaxed = plantMdelta(
+        E_gain=20, wI=1.0, sys_type='33_mult_unt', m_int=0.1, N_in=1, N_out=1)
+
+    noise_atten_func = Qsyn.lambda_log_interpolate(
+        [[0.1, 0.1], [25, 0.1], [25, 0.008], [100, 0.008]])
+
+    K_ad = co.c2d(co.tf([1], [2.5, 12, 0]), Ts)
+    K_Q = load_Qparam_controller('Nov21_ctrl.pkl')
+
+    # pre-processing
+    H_nom_ad = Ss.lft(Pz_nom, K_ad)
+    H_relaxed_ad = Ss.lft(Pz_relaxed, K_ad)
+    H_contracted_ad = Ss.lft(Pz_contracted, K_ad)
+
+    H_nom_Q = Ss.lft(Pz_nom, K_Q)
+    H_relaxed_Q = Ss.lft(Pz_relaxed, K_Q)
+    H_contracted_Q = Ss.lft(Pz_contracted, K_Q)
+
+    # fig init
+    fig = plt.figure(figsize=[7, 2.5])
+    ax0 = fig.add_subplot(2, 3, 1)
+    ax1 = fig.add_subplot(2, 3, 2, sharex=ax0, sharey=ax0)
+    ax2 = fig.add_subplot(2, 3, 3, sharex=ax0, sharey=ax0)
+    ax3 = fig.add_subplot(2, 3, 4, sharex=ax0)
+    ax4 = fig.add_subplot(2, 3, 5, sharex=ax0, sharey=ax3)
+    ax5 = fig.add_subplot(2, 3, 6, sharex=ax0, sharey=ax3)
+
+    # hide x labels
+    plt.setp(ax0.get_xticklabels(), visible=False)
+    plt.setp(ax1.get_xticklabels(), visible=False)
+    plt.setp(ax2.get_xticklabels(), visible=False)
+    plt.setp(ax1.get_yticklabels(), visible=False)
+    plt.setp(ax2.get_yticklabels(), visible=False)
+    plt.setp(ax4.get_yticklabels(), visible=False)
+    plt.setp(ax5.get_yticklabels(), visible=False)
+
+    ax3.set_yticks(np.pi * np.r_[-0.5, 0, 0.5])
+    ax3.set_yticklabels(['$-\pi/2$', '$0$', '$\pi/2$'])
+
+    # freq-response
+    freqs = np.linspace(1e-3, np.pi / Ts, 1000)
+    for ax in [ax0, ax1, ax2]:
+        # ax.set_xscale('log')
+        ax.set_yscale('log')
+
+    for ax in [ax3, ax4, ax5]:
+        # ax.plot([freqs[0], freqs[-1]], [np.pi / 2, np.pi / 2], '-', c='gray')
+        # ax.plot([freqs[0], freqs[-1]], [- np.pi / 2, - np.pi / 2], '-', c='gray')
+        ax.grid()
+        ax.set_xticks([0, 10, 20, 30, 40])
+
+    mag0, phase0, _ = H_relaxed_ad[0, 0].freqresp(freqs)
+    mag1, phase1, _ = H_nom_ad[0, 0].freqresp(freqs)
+    mag2, phase2, _ = H_contracted_ad[0, 0].freqresp(freqs)
+    ax0.plot(freqs, mag0[0, 0], label='ad.', linestyle='--')
+    ax1.plot(freqs, mag1[0, 0], label='ad.', linestyle='--')
+    ax2.plot(freqs, mag2[0, 0], label='ad.', linestyle='--')
+    ax3.plot(freqs, phase0[0, 0], label='ad.', linestyle='--')
+    ax4.plot(freqs, phase1[0, 0], label='ad.', linestyle='--')
+    ax5.plot(freqs, phase2[0, 0], label='ad.', linestyle='--')
+
+    mag0, phase0, _ = H_relaxed_Q[0, 0].freqresp(freqs)
+    mag1, phase1, _ = H_nom_Q[0, 0].freqresp(freqs)
+    mag2, phase2, _ = H_contracted_Q[0, 0].freqresp(freqs)
+    ax0.plot(freqs, mag0[0, 0], label='Q', linestyle='-.')
+    ax1.plot(freqs, mag1[0, 0], label='Q', linestyle='-.')
+    ax2.plot(freqs, mag2[0, 0], label='Q', linestyle='-.')
+    ax3.plot(freqs, phase0[0, 0], label='Q', linestyle='-.')
+    ax4.plot(freqs, phase1[0, 0], label='Q', linestyle='-.')
+    ax5.plot(freqs, phase2[0, 0], label='Q', linestyle='-.')
+
+    mag3 = noise_atten_func(freqs)
+    ax0.plot(freqs, mag3, label='bnd.', linestyle='-')
+    ax1.plot(freqs, mag3, label='bnd.', linestyle='-')
+    ax2.plot(freqs, mag3, label='bnd.', linestyle='-')
+
+    # post-process
+    for ax in [ax0, ax1, ax2]:
+        ax.grid(which='major')
+        ax.set_xlim(0, 45)
+        ax.set_ylim(0.005, 0.15)
+    ax3.set_ylim(-2.2, 2.2)
+    plt.tight_layout()
+    plt.savefig("Dec10_noise.pdf")
+    plt.show()
+
 
 if __name__ == '__main__':
     fig_admittance_shaping()
+    # fig_noise_attenuation()
